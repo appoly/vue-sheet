8cf8556eceb5c7885416d16add10866ddce6a20b
Calum Chamberlain <calum@appoly.co.uk>
2025-05-21 15:16:14 +0100

    build and publish

--- dist/vue-sheet.js
+++ dist/vue-sheet.js
@@ -1,10 +1,10 @@
-import { ref as f, computed as y, onMounted as M, onBeforeUnmount as $, watch as B, openBlock as l, createElementBlock as u, Fragment as E, renderSlot as h, createElementVNode as o, createCommentVNode as g, createBlock as _, Teleport as z, normalizeClass as I, normalizeStyle as W, nextTick as O } from "vue";
+import { ref as g, computed as y, onMounted as B, onBeforeUnmount as M, watch as $, openBlock as l, createElementBlock as u, Fragment as E, renderSlot as c, createElementVNode as o, createCommentVNode as f, createBlock as O, Teleport as _, normalizeClass as z, normalizeStyle as I, nextTick as W } from "vue";
 const j = (t, a) => {
   const e = t.__vccOpts || t;
-  for (const [c, r] of a)
-    e[c] = r;
+  for (const [h, d] of a)
+    e[h] = d;
   return e;
-}, H = ["aria-expanded"], T = { class: "sheet-buttons" }, L = { class: "sheet-content" }, V = {
+}, H = ["aria-expanded"], L = { class: "sheet-buttons" }, T = { class: "sheet-content" }, V = {
   __name: "Sheet",
   props: {
     position: {
@@ -17,7 +17,11 @@ const j = (t, a) => {
       default: "500px",
       validator: (t) => {
         const a = ["%", "px", "em", "rem", "vw", "vh"];
-        return a.some((e) => t.endsWith(e)) ? !0 : (console.error(`Invalid width: "${t}". Allowed endings are ${a.join(", ")}.`), !1);
+        return a.some((e) => t.endsWith(e)) ? !0 : (console.error(
+          `Invalid width: "${t}". Allowed endings are ${a.join(
+            ", "
+          )}.`
+        ), !1);
       }
     },
     height: {
@@ -25,11 +29,23 @@ const j = (t, a) => {
       default: "500px",
       validator: (t) => {
         const a = ["%", "px", "em", "rem", "vw", "vh"];
-        return a.some((e) => t.endsWith(e)) ? !0 : (console.error(`Invalid width: "${t}". Allowed endings are ${a.join(", ")}.`), !1);
+        return a.some((e) => t.endsWith(e)) ? !0 : (console.error(
+          `Invalid width: "${t}". Allowed endings are ${a.join(
+            ", "
+          )}.`
+        ), !1);
       }
     },
-    maxWidth: { type: String, default: "90%", validator: (t) => t.endsWith("%") },
-    maxHeight: { type: String, default: "90%", validator: (t) => t.endsWith("%") },
+    maxWidth: {
+      type: String,
+      default: "90%",
+      validator: (t) => t.endsWith("%")
+    },
+    maxHeight: {
+      type: String,
+      default: "90%",
+      validator: (t) => t.endsWith("%")
+    },
     open: { type: Boolean, default: !1 },
     closeOnEscape: { type: Boolean, default: !0 },
     noTrigger: { type: Boolean, default: !1 },
@@ -38,34 +54,40 @@ const j = (t, a) => {
     canClose: {
       type: Function,
       default: () => () => !0
-    }
+    },
+    disableOutsideScroll: { type: Boolean, default: !1 }
   },
   emits: ["update:open"],
   setup(t, { emit: a }) {
-    const e = t, c = a, r = f(!1), i = f(!1), d = f(e.expandable ? e.expandDefault : !1), k = `sheet-label-${Math.random().toString(36).substring(7)}`, x = `sheet-desc-${Math.random().toString(36).substring(7)}`, w = `sheet-${Math.random().toString(36).substring(7)}`, b = y(() => ["top", "bottom"].includes(e.position)), S = y(() => b.value ? { height: e.expandable && d.value ? "100vh" : e.height, maxHeight: e.maxHeight } : { width: e.expandable && d.value ? "100vw" : e.width, maxWidth: e.maxWidth }), p = () => {
-      r.value = !0, window.addEventListener("keydown", m), O(() => {
-        i.value = !0, c("update:open", !0);
+    const e = t, h = a, d = g(!1), i = g(!1), r = g(e.expandable ? e.expandDefault : !1), b = `sheet-label-${Math.random().toString(36).substring(7)}`, k = `sheet-desc-${Math.random().toString(36).substring(7)}`, w = `sheet-${Math.random().toString(36).substring(7)}`, x = y(
+      () => ["top", "bottom"].includes(e.position)
+    ), S = y(() => x.value ? { height: e.expandable && r.value ? "100vh" : e.height, maxHeight: e.maxHeight } : { width: e.expandable && r.value ? "100vw" : e.width, maxWidth: e.maxWidth }), p = () => {
+      d.value = !0, window.addEventListener("keydown", m), e.disableOutsideScroll && document.body.classList.add("sheet-open"), W(() => {
+        i.value = !0, h("update:open", !0);
       });
     }, v = async () => {
-      await e.canClose() && (window.removeEventListener("keydown", m), i.value = !1, c("update:open", !1));
+      await e.canClose() && (window.removeEventListener("keydown", m), e.disableOutsideScroll && document.body.classList.remove("sheet-open"), i.value = !1, h("update:open", !1));
     }, m = (n) => {
       (n.key === "Escape" || n.key === "Esc") && e.closeOnEscape && v();
     }, C = () => {
-      e.expandable && (d.value = !d.value);
+      e.expandable && (r.value = !r.value);
     };
-    return M(() => {
+    return B(() => {
       e.open && p();
-    }), $(() => {
+    }), M(() => {
       window.removeEventListener("keydown", m);
-    }), B(() => e.open, (n) => {
-      n && !i.value ? p() : !n && i.value && v();
-    }), (n, s) => (l(), u(E, null, [
-      t.noTrigger ? g("", !0) : (l(), u("div", {
+    }), $(
+      () => e.open,
+      (n) => {
+        n && !i.value ? p() : !n && i.value && v();
+      }
+    ), (n, s) => (l(), u(E, null, [
+      t.noTrigger ? f("", !0) : (l(), u("div", {
         key: 0,
         onClick: p,
-        onMouseover: s[0] || (s[0] = (A) => r.value = !0)
+        onMouseover: s[0] || (s[0] = (A) => d.value = !0)
       }, [
-        h(n.$slots, "trigger", {}, () => [
+        c(n.$slots, "trigger", {}, () => [
           o("button", {
             type: "button",
             class: "open-btn",
@@ -75,31 +97,31 @@ const j = (t, a) => {
           }, " Open ", 8, H)
         ], !0)
       ], 32)),
-      (l(), _(z, { to: "body" }, [
+      (l(), O(_, { to: "body" }, [
         i.value ? (l(), u("div", {
           key: 0,
           class: "overlay",
           onClick: v
-        })) : g("", !0),
-        r.value ? (l(), u("div", {
+        })) : f("", !0),
+        d.value ? (l(), u("div", {
           key: 1,
           id: w,
-          class: I(["sheet", t.position, { open: i.value }]),
+          class: z(["sheet", t.position, { open: i.value }]),
           role: "dialog",
           "aria-modal": "true",
           tabindex: "-1",
-          style: W(S.value),
-          "aria-labelledby": k,
-          "aria-describedby": x
+          style: I(S.value),
+          "aria-labelledby": b,
+          "aria-describedby": k
         }, [
-          o("div", T, [
+          o("div", L, [
             t.expandable ? (l(), u("button", {
               key: 0,
               class: "icon-btn",
               onClick: C,
               "aria-label": "Expand"
             }, [
-              d.value ? h(n.$slots, "minimize", { key: 1 }, () => [
+              r.value ? c(n.$slots, "minimize", { key: 1 }, () => [
                 s[2] || (s[2] = o("svg", {
                   xmlns: "http://www.w3.org/2000/svg",
                   width: "24",
@@ -117,7 +139,7 @@ const j = (t, a) => {
                   o("path", { d: "M3 16h3a2 2 0 0 1 2 2v3" }),
                   o("path", { d: "M16 21v-3a2 2 0 0 1 2-2h3" })
                 ], -1))
-              ], !0) : h(n.$slots, "maximize", { key: 0 }, () => [
+              ], !0) : c(n.$slots, "maximize", { key: 0 }, () => [
                 s[1] || (s[1] = o("svg", {
                   xmlns: "http://www.w3.org/2000/svg",
                   width: "24",
@@ -136,13 +158,13 @@ const j = (t, a) => {
                   o("path", { d: "M16 21h3a2 2 0 0 0 2-2v-3" })
                 ], -1))
               ], !0)
-            ])) : g("", !0),
+            ])) : f("", !0),
             o("button", {
               class: "icon-btn",
               onClick: v,
               "aria-label": "Close"
             }, [
-              h(n.$slots, "close", {}, () => [
+              c(n.$slots, "close", {}, () => [
                 s[3] || (s[3] = o("svg", {
                   xmlns: "http://www.w3.org/2000/svg",
                   width: "24",
@@ -161,14 +183,14 @@ const j = (t, a) => {
               ], !0)
             ])
           ]),
-          o("div", L, [
-            h(n.$slots, "default", {}, void 0, !0)
+          o("div", T, [
+            c(n.$slots, "default", {}, void 0, !0)
           ])
-        ], 6)) : g("", !0)
+        ], 6)) : f("", !0)
       ]))
     ], 64));
   }
-}, F = /* @__PURE__ */ j(V, [["__scopeId", "data-v-ad7ec866"]]);
+}, F = /* @__PURE__ */ j(V, [["__scopeId", "data-v-e01f7da9"]]);
 export {
   F as Sheet
 };
